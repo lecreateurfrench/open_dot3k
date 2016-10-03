d453fa565600968f148810b34b599e40c35a2ad4
arceus <arceus@minet.net>
2016-10-03 22:40:07 +0200

    backlight corrected

--- open_dot3k/backlight.py
+++ open_dot3k/backlight.py
@@ -7,17 +7,19 @@ class Backlight():
 	def __init__(self):
 		self.maxtemp = 50.0
 		self.mintemp = 10.0
+		self.midtemp = 30.0
 		self.step = 255.0 / ((self.maxtemp - self.mintemp)/2)
 
 	def color(self, temp):
-		increment = int((temp - self.mintemp)  * self.step)
 		try: 
-			if temp <= 25:
+			if temp <= self.midtemp:
+				increment = int((temp - self.mintemp)  * self.step)
 				green = 0 + increment
 				blue = 255 - increment
 				red = 0
 				backlight.rgb(red, green, blue)
-			elif temp > 25:
+			elif temp > self.midtemp:
+				increment = int((temp - self.midtemp)  * self.step)
 				green = 255 - increment
 				red = 0 + increment
 				blue = 0
